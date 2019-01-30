b9776ee316d68b122a547b66fdb29be4dea84272
Dominik <dominik.nowak@husarion.com>
2019-01-30 20:35:45 +0100

    initial commit

---
+++ ESP32-to-ESP32.ino
@@ -0,0 +1,208 @@
+#include <WiFi.h>
+#include <Husarnet.h>
+#include <AceButton.h>
+
+using namespace ace_button;
+
+/* =============== config section start =============== */
+
+#define DEV_TYPE 0    // type "0" for 1st ESP32, and "1" for 2nd ESP32
+
+const int BUTTON_PIN = 0;
+const int LED_PIN = 23;
+
+// Husarnet credentials
+const char* hostName0 = "esp2esp0";  //this will be the name of the 1st ESP32 device at https://app.husarnet.com
+const char* hostName1 = "esp2esp1";  //this will be the name of the 2nd ESP32 device at https://app.husarnet.com
+
+/* to get your join code go to https://app.husarnet.com
+   -> select network
+   -> click "Add element"
+   -> select "join code" tab
+
+   Keep it secret!
+*/
+const char* husarnetJoinCode = "xxxxxxxxxxxxxxxxxxxxxx";
+
+// WiFi credentials
+#define NUM_NETWORKS 2  //number of Wi-Fi network credentials saved
+
+const char* ssidTab[NUM_NETWORKS] = {
+  "wifi-ssid-one",
+  "wifi-ssid-two",
+};
+
+const char* passwordTab[NUM_NETWORKS] = {
+  "wifi-pass-one",
+  "wifi-pass-two",
+};
+
+/* =============== config section end =============== */
+
+AceButton btn(BUTTON_PIN);
+
+HusarnetClient client;
+
+uint16_t port = 8001;
+
+#if DEV_TYPE == 0
+HusarnetServer server(port);
+#endif
+
+void handleEvent(AceButton*, uint8_t, uint8_t);
+void taskWifi( void * parameter );
+void taskConnection( void * parameter );
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  btn.setEventHandler(handleEvent);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+
+  xTaskCreate(
+    taskWifi,          /* Task function. */
+    "taskWifi",        /* String with name of task. */
+    10000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+
+  xTaskCreate(
+    taskConnection,          /* Task function. */
+    "taskConnection",        /* String with name of task. */
+    10000,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+}
+
+void loop() {
+  while (1) {
+    btn.check();
+    delay(1);
+  }
+}
+
+void handleEvent(AceButton* button, uint8_t eventType,
+                 uint8_t buttonState) {
+  switch (eventType) {
+    case AceButton::kEventPressed:
+      Serial.println("pressed");
+      break;
+    case AceButton::kEventReleased:
+      Serial.println("released");
+      break;
+  }
+}
+
+void taskWifi( void * parameter ) {
+  while (1) {
+    for (int i = 0; i < NUM_NETWORKS; i++) {
+      Serial.print("Connecting to ");
+      Serial.print(ssidTab[i]);
+      WiFi.begin(ssidTab[i], passwordTab[i]);
+      for (int j = 0; j < 10; j++) {
+        if (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+        } else {
+          Serial.println("done");
+          Serial.print("IP address: ");
+          Serial.println(WiFi.localIP());
+#if DEV_TYPE == 0
+          Husarnet.join(husarnetJoinCode, hostName0);
+#elif DEV_TYPE == 1
+          Husarnet.join(husarnetJoinCode, hostName1);
+#endif
+          Husarnet.start();
+          while (WiFi.status() == WL_CONNECTED) {
+            delay(500);
+          }
+        }
+      }
+    }
+  }
+}
+
+void taskConnection( void * parameter ) {
+  uint8_t oldState = btn.getLastButtonState();
+
+  while (1) {
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+
+#if DEV_TYPE == 0
+
+    server.begin();
+    Serial.println("Waiting for client");
+
+    do {
+      delay(500);
+      client = server.available();
+      client.setTimeout(3);
+    } while (client < 1);
+
+#elif DEV_TYPE == 1
+
+    Serial.printf("Connecting to %s\r\n", hostName0);
+    while (client.connect(hostName0, port) == 0) {
+      delay(500);
+    }
+
+#endif
+
+    Serial.printf("Client connected: %d\r\n", (int)client.connected());
+
+    unsigned long lastMsg = millis();
+    auto lastPing = 0;
+
+    while (client.connected()) {
+      //ping RX
+      if (millis() - lastMsg > 6000) {
+        Serial.println("ping timeout");
+        break;
+      }
+
+      //ping TX
+      auto now = millis();
+      if (now > lastPing + 4000) {
+        client.print('p');
+        lastPing = now;
+      }
+
+      if (oldState != btn.getLastButtonState()) {
+        oldState = btn.getLastButtonState();
+        if (oldState == 0) {
+          client.print('a');
+          digitalWrite(LED_PIN, HIGH);
+        } else {
+          client.print('b');
+          digitalWrite(LED_PIN, LOW);
+        }
+      }
+
+      if (client.available()) {
+        char c = client.read();
+
+        if (c == 'p') {
+          lastMsg = millis();
+        }
+        if (c == 'a') {
+          digitalWrite(LED_PIN, HIGH);
+        }
+        if (c == 'b') {
+          digitalWrite(LED_PIN, LOW);
+        }
+
+        Serial.printf("read: %c\r\n", c);
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ESP32-to-ESP32
-ESP32 to ESP32 communication example using Arduino framework. Secure, P2P, low latency connection between devices is established. Button connected to the first ESP32 controlls LED connected to second ESP32.
+*ESP32 to ESP32 communication example using Arduino framework. Secure, P2P, low latency connection between devices is established. Button connected to the first ESP32 controlls LED connected to second ESP32.*
+
+Open Arduino IDE and follow these steps:
+
+**1. Install Husarnet IDF for ESP32:**
+* open ```File -> Preferences```
+* in a field **Additional Board Manager URLs** add this link: `https://files.husarion.com/arduino/package_esp32_index.json`
+* open ```Tools -> Board: ... -> Boards Manager ...```
+* Search for `esp32-husarnet by Husarion`
+* Click Install button
+
+**2. Select ESP32 dev board:**
+* open ```Tools -> Board```
+* select ***ESP32 Dev Module*** under "ESP32 Arduino (Husarnet)" section
+
+**3. Run demo:**
+* Open **ESP32_ledstrip_webserver.ino** project
+* modify line 25 with your Husarnet join code (get on https://app.husarnet.com)
+* modify lines 28 - 38 to add your Wi-Fi network credentials
+* upload project to your ESP32 board. Repeat the previous steps with ```#define DEV_TYPE 1``` for 2nd ESP32 module
+* power both ESP32 modules and wait about 15 seconds to allow your ESP32 devices connect to Wi-Fi network and establish P2P connection (works both in LAN and through the internet).
+
+
+
