6c8f49a95696bdc76906eca4fb4c2d798056f81c
dominikn <dominik.nwk@gmail.com>
2021-07-08 22:06:06 +0200

    clean up a project a little bit

--- src/ESP32-to-ESP32.ino
+++ src/ESP32-to-ESP32.ino
@@ -15,10 +15,6 @@ using namespace ace_button;
 
 #define ENABLE_TFT 1  // tested on TTGO T Display
 
-const int BUTTON_PIN = 0;
-const int LED_PIN = 27;
-const int PORT = 8001;
-
 #if __has_include("credentials.h")
 #include "credentials.h"
 #else
@@ -69,38 +65,119 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
   { Serial.printf((f_), ##__VA_ARGS__); }
 #endif
 
+const int BUTTON_PIN = 0;
+const int LED_PIN = 27;
+const int PORT = 8001;
+
+int ledState = 0;
+
+IPAddress myip;
+
+// Push button
 AceButton btn(BUTTON_PIN);
+void handleButtonEvent(AceButton *, uint8_t, uint8_t);
 
 // you can provide credentials to multiple WiFi networks
 WiFiMulti wifiMulti;
 
-/* store index.html content in html constant variable (platformio feature) */
+// store index.html content in html constant variable (platformio feature)
 extern const char index_html_start[] asm("_binary_src_index_html_start");
 const String html = String((const char*)index_html_start);
 AsyncWebServer server(PORT);
 
-// TCP client array for HTTP requests to other Peers after button press/release
-
-IPAddress myip;
-
-void handleEvent(AceButton *, uint8_t, uint8_t);
+// Task functions
 void taskWifi(void *parameter);
-void taskConnection(void *parameter);
 
-int ledState = 0;
+void setup() {
+  Serial.begin(115200);
 
-void handleEvent(AceButton *button, uint8_t eventType, uint8_t buttonState) {
+#if ENABLE_TFT == 1
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(1);
+#endif
 
-  switch (eventType) {
-    case AceButton::kEventPressed:
-      Serial.printf("\r\n===========PRESSED============\r\n");
-      ledState = 1;
-      break;
-    case AceButton::kEventReleased:
-      Serial.printf("\r\n===========RELEASED===========\r\n");
-      ledState = 0;
-      break;
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  btn.setEventHandler(handleButtonEvent);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
+  xTaskCreate(taskWifi,   /* Task function. */
+              "taskWifi", /* String with name of task. */
+              10000,      /* Stack size in bytes. */
+              NULL,       /* Parameter passed as input of the task */
+              1,          /* Priority of the task. */
+              NULL);      /* Task handle. */
+}
+
+void loop() {
+  while (1) {
+    btn.check();
+    delay(1);
   }
+}
+
+void taskWifi(void *parameter) {
+  uint8_t stat = WL_DISCONNECTED;
+
+  /* Configure Wi-Fi */
+  for (int i = 0; i < NUM_NETWORKS; i++) {
+    wifiMulti.addAP(ssidTab[i], passwordTab[i]);
+    Serial.printf("WiFi %d: SSID: \"%s\" ; PASS: \"%s\"\r\n", i, ssidTab[i], passwordTab[i]);
+  }
+
+  while (stat != WL_CONNECTED) {
+    stat = wifiMulti.run();
+    Serial.printf("WiFi status: %d\r\n", (int)stat);
+    delay(100);
+  }
+
+  Serial.printf("WiFi connected\r\n", (int)stat);
+  Serial.printf("IP address: ");
+
+  myip = WiFi.localIP();
+  LOG("IP: %u.%u.%u.%u\r\n", myip[0], myip[1], myip[2], myip[3]);
+
+  /* Start Husarnet */
+  Husarnet.selfHostedSetup(dashboardURL);
+  Husarnet.join(husarnetJoinCode, hostname);
+  Husarnet.start();
+  LOG("Hostname: %s\r\n--\r\n", hostname);
+
+    /* Start a web server */
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(200, "text/html", html);
+  });
+
+  // Send a GET request to <IP>/led/<number>/state/<0 or 1>
+  server.on("^\\/led\\/([0-9]+)\\/state\\/([0-9]+)$", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String ledNumber = request->pathArg(0); // currently unused - we use only a predefined LED number
+    String state = request->pathArg(1);
+
+    digitalWrite(LED_PIN, state.toInt());
+    request->send(200, "text/plain", "LED: " + ledNumber + ", with state: " + state);
+  });
+
+  server.begin();
+
+  uint8_t oldState = btn.getLastButtonState();
+
+  while (1) {
+    while (WiFi.status() == WL_CONNECTED) {
+      delay(500);
+    }
+    LOG("WiFi disconnected, reconnecting\r\n");
+    delay(500);
+    stat = wifiMulti.run();
+    LOG("WiFi status: %d\r\n", (int)stat);
+  }
+}
+
+void handleButtonEvent(AceButton *button, uint8_t eventType, uint8_t buttonState) {
+  ledState = buttonState;
 
   for (auto const &host : Husarnet.listPeers()) {
     IPv6Address peerAddr = host.first;
@@ -144,96 +221,5 @@ void handleEvent(AceButton *button, uint8_t eventType, uint8_t buttonState) {
       
       client_tcp->connect(peerAddr, PORT);
     }
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-
-#if ENABLE_TFT == 1
-  tft.init();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.setTextSize(1);
-#endif
-  //--------------------
-
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  btn.setEventHandler(handleEvent);
-
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
-
-  xTaskCreate(taskWifi,   /* Task function. */
-              "taskWifi", /* String with name of task. */
-              10000,      /* Stack size in bytes. */
-              NULL,       /* Parameter passed as input of the task */
-              1,          /* Priority of the task. */
-              NULL);      /* Task handle. */
-}
-
-void loop() {
-  while (1) {
-    btn.check();
-    delay(1);
-  }
-}
-
-void taskWifi(void *parameter) {
-  uint8_t stat = WL_DISCONNECTED;
-
-  /* Configure Wi-Fi */
-  for (int i = 0; i < NUM_NETWORKS; i++) {
-    wifiMulti.addAP(ssidTab[i], passwordTab[i]);
-    Serial.printf("WiFi %d: SSID: \"%s\" ; PASS: \"%s\"\r\n", i, ssidTab[i],
-                  passwordTab[i]);
-  }
-
-  while (stat != WL_CONNECTED) {
-    stat = wifiMulti.run();
-    Serial.printf("WiFi status: %d\r\n", (int)stat);
-    delay(100);
-  }
-
-  Serial.printf("WiFi connected\r\n", (int)stat);
-  Serial.printf("IP address: ");
-
-  myip = WiFi.localIP();
-  LOG("IP: %u.%u.%u.%u\r\n", myip[0], myip[1], myip[2], myip[3]);
-
-  /* Start Husarnet */
-  Husarnet.selfHostedSetup(dashboardURL);
-  Husarnet.join(husarnetJoinCode, hostname);
-  Husarnet.start();
-  LOG("Hostname: %s\r\n--\r\n", hostname);
-
-    /* Start a web server */
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-    request->send(200, "text/html", html);
-  });
-
-  // Send a GET request to <IP>/led/<number>/state/<0 or 1>
-  server.on("^\\/led\\/([0-9]+)\\/state\\/([0-9]+)$", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String ledNumber = request->pathArg(0); // currently unused - we use only a predefined LED number
-    String state = request->pathArg(1);
-
-    digitalWrite(LED_PIN, state.toInt());
-    Serial.printf("\r\n<< SERVER_HANDLER: %d >>\r\n", state.toInt());
-    request->send(200, "text/plain", "LED: " + ledNumber + ", with state: " + state);
-  });
-
-  server.begin();
-
-  uint8_t oldState = btn.getLastButtonState();
-
-  while (1) {
-    while (WiFi.status() == WL_CONNECTED) {
-      delay(500);
-    }
-    LOG("WiFi disconnected, reconnecting\r\n");
-    delay(500);
-    stat = wifiMulti.run();
-    LOG("WiFi status: %d\r\n", (int)stat);
   }
 }
